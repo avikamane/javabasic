e4a039fa8a6466f360d3427b4a606057f2cbacb8
avikamane <avika.mane.cc@gmail.com>
2026-06-06 12:55:15 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,23 @@
+//basic code in JavaScript
+console.log("Hello World!");
+
+//variable declaration in JavaScript
+Name = "Avika Mane"
+console.log(Name);
+
+age = 18;
+console.log(age);
+
+a = 10;
+b = 20;
+c = null;
+d = undefined;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+//let and const in JavaScript
+let city = "Pune";
+console.log(city);
